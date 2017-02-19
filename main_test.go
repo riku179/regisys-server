4a7dd0dba639419807fad152b1cf93778d894fff
riku <riku@mma.club.uec.ac.jp>
2017-02-19 17:07:38 +0900

    Fix

--- main_test.go
+++ main_test.go
@@ -14,25 +14,13 @@ import (
 )
 
 var (
-	ctx    context.Context = context.Background()
-	testPW                 = "foobar" // password for all testUser
-	//testUser                 = map[string]string{
-	//	"MMA":  "Mr.MMA",       // user of MMA member in 'Normal' group
-	//	"MMA2": "Mr.MMA2",      // second person in same group
-	//	"OB":   "Mr.OB",        // OB user(not MMA member) in 'Normal' group
-	//	"OB2":  "Mr.OB2",       // second person in same group
-	//	"Reg":  "Mr.Register",  // user of 'Register' group
-	//	"Reg2": "Mr.Register2", // second person in same group
-	//	"Adm":  "Mr.Admin",     // user of 'Admin' group
-	//	"Adm2": "Mr.Admin2",    // second person in same group
-	//}
+	ctx context.Context = context.Background()
 )
 
 // ## 目次
 // * DBへの接続
 // * テスト用テーブルの作成の作成・マイグレーション
 // * ミドルウェア・コントローラの登録
-// * テスト用データのINSERT
 // * +テストの実行+
 // * テスト用テーブルのDROP
 func TestMain(m *testing.M) {
@@ -73,9 +61,6 @@ func TestMain(m *testing.M) {
 	c5 := NewUserController(service)
 	app.MountUserController(service, c5)
 
-	// * テスト用データのINSERT
-	insertTestUser()
-
 	// * +テストの実行+
 	code := m.Run()
 
@@ -83,60 +68,4 @@ func TestMain(m *testing.M) {
 	db.DropTableIfExists(ItemsDB.TableName(), UserDB.TableName(), OrdersDB.TableName())
 
 	os.Exit(code)
-}
-
-func insertTestUser() {
-	//bPW, _ := bcrypt.GenerateFromPassword([]byte(testPW), 10)
-	//pw := string(bPW)
-
-	////一般権限のMMA部員
-	//UserDB.Add(ctx, &models.User{
-	//	IsMember: true,
-	//	Name:     testUser["MMA"],
-	//	Password: pw,
-	//	Group:    Normal,
-	//})
-	//UserDB.Add(ctx, &models.User{
-	//	IsMember: true,
-	//	Name:     testUser["MMA2"],
-	//	Password: pw,
-	//	Group:    Normal,
-	//})
-	////　OB
-	//UserDB.Add(ctx, &models.User{
-	//	IsMember: false,
-	//	Name:     testUser["OB"],
-	//	Password: pw,
-	//})
-	//UserDB.Add(ctx, &models.User{
-	//	IsMember: false,
-	//	Name:     testUser["OB2"],
-	//	Password: pw,
-	//})
-	//// レジ打ち権限のMMA部員
-	//UserDB.Add(ctx, &models.User{
-	//	IsMember: true,
-	//	Name:     testUser["Reg"],
-	//	Password: pw,
-	//	Group:    Register,
-	//})
-	//UserDB.Add(ctx, &models.User{
-	//	IsMember: true,
-	//	Name:     testUser["Reg2"],
-	//	Password: pw,
-	//	Group:    Register,
-	//})
-	//// 管理者権限のMMA部員
-	//UserDB.Add(ctx, &models.User{
-	//	IsMember: true,
-	//	Name:     testUser["Adm"],
-	//	Password: pw,
-	//	Group:    Admin,
-	//})
-	//UserDB.Add(ctx, &models.User{
-	//	IsMember: true,
-	//	Name:     testUser["Adm2"],
-	//	Password: pw,
-	//	Group:    Admin,
-	//})
 }
